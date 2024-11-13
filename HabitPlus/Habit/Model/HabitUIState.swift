f8f636c7a92c83d1cdf70d5909fbe2b5f017d9de
Lucas Oliveira <lucas.firmino@>
2024-11-12 22:47:42 -0300

    feat: implement view habit

---
+++ HabitPlus/Habit/Model/HabitUIState.swift
@@ -0,0 +1,15 @@
+//
+//  HabitModel.swift
+//  HabitPlus
+//
+//  Created by Lucas De Oliveira Silva Firmino on 12/11/24.
+//
+
+import Foundation
+
+enum HabitUIState {
+    case loading
+    case empytList
+    case fullList
+    case error(String)
+}
